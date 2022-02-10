f3b6111e101aaabfcfcdc3add5c4e8537ab65d7c
fdjs-rotto <root@localhost.localdomain>
2022-02-10 21:27:36 +0800

    workman中文

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'workerman/workerman' => 
+    array (
+      'pretty_version' => 'v4.0.26',
+      'version' => '4.0.26.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '27573e9f985f9ec0665b1f9924308d359bd0fdaa',
+    ),
+  ),
+);
